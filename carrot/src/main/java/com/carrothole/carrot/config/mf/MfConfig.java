d42bfca067563f0ba0cc1600f7c9eea81f1e91b3
moon <it_moon@126.com>
2024-09-01 17:21:21 +0800

    build(test): 打包报错问题

---
+++ carrot/src/main/java/com/carrothole/carrot/config/mf/MfConfig.java
@@ -0,0 +1,41 @@
+package com.carrothole.carrot.config.mf;
+
+import com.mybatisflex.core.FlexGlobalConfig;
+import com.mybatisflex.core.audit.AuditManager;
+import com.mybatisflex.core.audit.ConsoleMessageCollector;
+import com.mybatisflex.core.audit.MessageCollector;
+import com.mybatisflex.core.keygen.KeyGeneratorFactory;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * ClassName: MfConfig
+ * Description:
+ * date: 2024-9-1 16:31
+ *
+ * @author moon
+ * @since JDK 1.8
+ */
+
+public class MfConfig implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        mybatisFlexConfig();
+    }
+
+    public static void mybatisFlexConfig(){
+        System.out.println("mybatis config init");
+        FlexGlobalConfig.getDefaultConfig().setTenantColumn("tenant_id");
+        KeyGeneratorFactory.register(MfConstant.ID_GENERATOR, new MfUUIDGenerator());
+
+        //开启审计功能
+        AuditManager.setAuditEnable(true);
+
+        //设置 SQL 审计收集器
+        MessageCollector collector = new ConsoleMessageCollector();
+        AuditManager.setMessageCollector(collector);
+    }
+
+}
